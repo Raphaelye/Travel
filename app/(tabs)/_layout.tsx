103c4dfa080713656f8acb28034ba5c4e00a3821
Kesit Udofia <kesitpatrick@gmail.com>
2024-06-03 18:16:02 +0100

    searchbar done

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,31 +3,33 @@ import React from 'react'
 import { Tabs } from 'expo-router'
 import { Ionicons } from '@expo/vector-icons'
 import Colors from '@/constants/Colors'
+import { coolDownAsync } from 'expo-web-browser'
+
 
 export default function RootLayout() {
   return (
     <Tabs screenOptions={{tabBarStyle:{
-        backgroundColor: Colors.bgColor,
-        borderTopWidth: 0,
-        padding: 0,
-      }, 
+          backgroundColor: Colors.bgColor,
+          borderTopWidth: 0,
+          padding: 0,
+        },
       tabBarShowLabel:false,
-      tabBarActiveTintColor:"black",
-      
+      tabBarActiveTintColor:"#333",
+      tabBarInactiveTintColor: Colors.grey
     }}>
     
     {/* Setting up the various tabs  */}
-      <Tabs.Screen name = 'index' options={{ tabBarIcon: (color) => (
-        <Ionicons name='compass' size={25} color={Colors.grey} />
+      <Tabs.Screen name = 'index' options={{ tabBarIcon: ({ color }) => (
+        <Ionicons name='compass' size={25} color={color}  />
       )}} />
 
-      <Tabs.Screen name = 'category' options={{ tabBarIcon: (color) => (
-        <Ionicons name='grid' size={25} color={Colors.grey} /> 
+      <Tabs.Screen name = 'category' options={{ tabBarIcon: ({ focused }) => (
+        <Ionicons name='grid' size={25} color={focused ? Colors.black : Colors.grey}/> 
       )}} />
 
-      <Tabs.Screen name = 'search' options={{ tabBarIcon: (color) => (
+      <Tabs.Screen name = 'search' options={{ tabBarIcon: ({color}) => (
         <View style={{ 
-            backgroundColor:Colors.primanryColor, 
+            backgroundColor:Colors.primanryColor,
             paddingHorizontal:16, 
             paddingVertical:12, 
             borderRadius:10, 
@@ -37,12 +39,12 @@ export default function RootLayout() {
         </View>
       )}} />
 
-      <Tabs.Screen name = 'bookmarks'  options={{ tabBarIcon: (color) => (
-        <Ionicons name='bookmark' size={25} color={Colors.grey} /> 
+      <Tabs.Screen name = 'bookmarks'  options={{ tabBarIcon: ({ color, focused }) => (
+        <Ionicons name='bookmark' size={25} color={color} /> 
       )}} />
       
-      <Tabs.Screen name = 'profile' options={{ tabBarIcon: (color) => (
-        <Ionicons name='person' size={25} color={Colors.grey} /> 
+      <Tabs.Screen name = 'profile' options={{ tabBarIcon: ({ color, focused }) => (
+        <Ionicons name='person' size={25} color={color} />
       )}} /> 
    </Tabs>
   )
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,66 +1,111 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View, TextInput} from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
-import { TouchableOpacity } from 'react-native-gesture-handler'
+import { TouchableOpacity } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 import Colors from '@/constants/Colors'
+import {useHeaderHeight} from "@react-navigation/elements"
+
 
 const Page = () => {
+
+  const headerHeight = useHeaderHeight();
   return (
-    <Stack.Screen options={{
-      headerTransparent:true,
-      headerTitle:'',
-      headerLeft: () => (
-        <TouchableOpacity style={{marginLeft:20}} onPress={() => {}}>
-          <Image 
-            source={{
-              uri:"https://xsgames.co/randomusers/assets/avatars/male/61.jpg" 
-            }}
-            
-            style={{
-              width:40,
-              height:40,
-              borderRadius:10
-            }}
-          />
-        </TouchableOpacity>
-      ),
-      headerRight: () => (
-        <TouchableOpacity onPress={() => {}} style={headerStyle.header}>
-          <Ionicons name='notifications' size={24}/>
-          
-        </TouchableOpacity>
-      )
-    }}>
-    </Stack.Screen>
+    <>
+      <Stack.Screen options={{
+        headerTransparent:true,
+        headerTitle:'',
+        headerLeft: () => (
+          <TouchableOpacity style={{marginLeft:20, marginTop:12}} onPress={() => {}}>
+            <Image 
+              source={{
+                uri:"https://xsgames.co/randomusers/assets/avatars/male/61.jpg" 
+              }}
+              
+              style={{
+                width:36,
+                height:36,
+                borderRadius:10
+              }}
+            />
+          </TouchableOpacity>
+        ),
+        headerRight: () => (
+          <TouchableOpacity onPress={() => {}} style={styles.header}>
+            <Ionicons name='notifications' size={17}/>
+          </TouchableOpacity>
+        )
+      }}/>
+      <View style={[styles.container, {paddingTop:headerHeight}]}> 
+        {/* <Text style={styles.headingTxt}>Explore the Beautiful World!!</Text> */}
+        <View style={styles.searchBarContainer}> 
+          <View style={styles.searchBar}>
+            <Ionicons name='search' size={20} style={{marginRight:10}} color={Colors.black}></Ionicons>
+            <TextInput placeholder="Search..."/>
+          </View>
+          <TouchableOpacity onPress={() => {}} style={styles.filterButton}>
+            <Ionicons name='options' size={28} color={Colors.white}/>
+          </TouchableOpacity>
+        </View>
+      </View>        
+    </>
   )
 }
 
 export default Page
 
 const styles = StyleSheet.create({
-    container: {
-      flex:1,
-      justifyContent:'center',
-      alignItems:'center',
-    }
-})
-
-const headerStyle = StyleSheet.create({
+  container: {
+    flex:1,
+    paddingHorizontal:20,
+    marginTop: 10,
+  },
+  
   header:{
     marginRight:20,
     backgroundColor:Colors.white,
-    paddingHorizontal:14,
-    paddingVertical:12,
+    paddingHorizontal:9,
+    paddingVertical:9,
     borderRadius:10,
     marginTop:10,
     shadowColor: "#000",
     shadowOffset: {
       width: 1,
-      height: 3,
+      height: 2,
     },
     shadowOpacity: 0.30,
-    shadowRadius: 4,
+    shadowRadius: 2,
     elevation: 8,
+  },
+
+  headingTxt:{
+    fontSize:28,
+    fontWeight:900,
+  },
+
+  searchBarContainer:{
+    flexDirection:"row",
+    marginVertical:16,
+  },
+
+  searchBar:{
+    flex:1,
+    flexDirection:"row",
+    backgroundColor:Colors.white,
+    padding:16,
+    borderRadius:10,
+    
+  },
+
+  filterButton:{
+    backgroundColor:Colors.primanryColor,
+    padding:10,
+    borderRadius:10,
+    marginLeft: 20,
+
   }
+
+  
+
 })
+
